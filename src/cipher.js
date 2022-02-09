040cd88a7ebf5fae048b0237db93ac3cbcea97c1
carmen0810 <66385122+Hiald@users.noreply.github.com>
2022-02-08 20:02:00 -0500

    encode y decode Cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,32 @@
 const cipher = {
-  // ...
-};
+  encode: function (offset, string) {
+    document.getElementById("resultadoEncode").innerHTML = "";
+    for (let i = 0; i < string.value.length; i++) {
+      let letraEnNumero = (string.value[i].charCodeAt()-65+offset)%26+65;
+      document.getElementById("resultadoEncode").append(String.fromCharCode(letraEnNumero));
+    }
+    
+  },
+  decode: function (offset, string) {
+    document.getElementById("resultadoDecode").innerHTML = "";
+    for (let i = 0; i < string.value.length; i++) {
+      let letraEnNumero = (string.value[i].charCodeAt()-65-offset+26)%26+65;
+      
+       
+      if ((string.value[i].charCodeAt()-65-offset)%26+65 == 88 || (string.value[i].charCodeAt()-65-offset)%26+65 == 89|| (string.value[i].charCodeAt()-65-offset)%26+65 == 90){
+        (string.value[i].charCodeAt()-65-offset+26)%26+65;
+        document.getElementById("resultadoDecode").append(String.fromCharCode(letraEnNumero));
+      }else{
+        (string.value[i].charCodeAt()-65-offset)%26+65;
+        document.getElementById("resultadoDecode").append(String.fromCharCode(letraEnNumero));
+      }
 
+    
+    
+    
+    
+    }
+    
+  }
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import cipher from './cipher.js';
-
-console.log(cipher);
+let encode1 = document.getElementById("cifrado");
+let decode1 = document.getElementById("decifrado");
+function encodeCifrado() {
+    cipher.encode(3, encode1);
+}
+function decodeDecifrado() {
+    cipher.decode(3, decode1);
+}
+document.getElementById("encode").addEventListener("click", encodeCifrado);
+document.getElementById("decode").addEventListener("click", decodeDecifrado);
